4f6c8c59a3f2c67686b72c6d22864a767f232095
Tom Flowerdew <tom@Fortify-Tom-MacBook-Pro.local>
2026-01-22 13:28:11 +0000

    Update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/interactive-explorer/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/gtm-explorer/' : '/',
 })
